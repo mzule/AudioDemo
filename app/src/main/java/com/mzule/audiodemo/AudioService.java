b4ab497149743395d9974d540fb9652c8aecdfff
Jason Cao <mzule4j@gmail.com>
2014-12-23 13:47:29 +0800

    play audio under service

---
+++ app/src/main/java/com/mzule/audiodemo/AudioService.java
@@ -0,0 +1,123 @@
+package com.mzule.audiodemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+ * Created by Lennnna on 12/23/14.
+ */
+public class AudioService extends Service implements MediaPlayer.OnCompletionListener {
+    private static final int PROGRESS_UPDATE_PERIOD = 100;
+    private static final int MSG_UPDATE_PROGRESS = 1;
+
+    private static final String TAG = AudioService.class.getSimpleName();
+    public static final String ACTION_PLAY = "com.mzule.audiodemo.PLAY";
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    private MediaPlayer.OnErrorListener onErrorListener;
+    private MediaPlayer.OnBufferingUpdateListener onBufferingUpdateListener;
+
+    private OnAudioProgressUpdateListener onAudioProgressUpdateListener;
+    private int duration;
+    private boolean isDestroyed;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (onAudioProgressUpdateListener != null) {
+                Log.d(TAG, "onAudioProgressUpdateListener");
+                onAudioProgressUpdateListener.update(duration, mediaPlayer.getCurrentPosition());
+            }
+            if (!isDestroyed) {
+                handler.sendEmptyMessageDelayed(MSG_UPDATE_PROGRESS, PROGRESS_UPDATE_PERIOD);
+            }
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind");
+        return new AudioBinder();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        final String url = intent.getStringExtra("url");
+        mediaPlayer.stop();
+        mediaPlayer.reset();
+        new Thread(new Runnable() {
+            @Override public void run() {
+                if (ACTION_PLAY.equals(intent.getAction())) {
+                    Log.d(TAG, "onStartCommand#run#action_play");
+                    try {
+                        mediaPlayer.setDataSource(url);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                        mediaPlayer.setOnCompletionListener(AudioService.this);
+                        mediaPlayer.setOnErrorListener(onErrorListener);
+                        mediaPlayer.setOnBufferingUpdateListener(onBufferingUpdateListener);
+                        duration = mediaPlayer.getDuration();
+                        handler.sendEmptyMessage(MSG_UPDATE_PROGRESS);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        stopSelf();
+        Log.d(TAG, "onCompletion");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        isDestroyed = true;
+        Log.d(TAG, "onDestroy");
+    }
+
+    public void setOnErrorListener(MediaPlayer.OnErrorListener onErrorListener) {
+        this.onErrorListener = onErrorListener;
+    }
+
+    public void setOnBufferingUpdateListener(MediaPlayer.OnBufferingUpdateListener onBufferingUpdateListener) {
+        this.onBufferingUpdateListener = onBufferingUpdateListener;
+    }
+
+    public void setOnAudioProgressUpdateListener(OnAudioProgressUpdateListener onAudioProgressUpdateListener) {
+        this.onAudioProgressUpdateListener = onAudioProgressUpdateListener;
+    }
+
+    public void setListener(Object listener) {
+        setOnErrorListener((MediaPlayer.OnErrorListener) listener);
+        setOnBufferingUpdateListener((MediaPlayer.OnBufferingUpdateListener) listener);
+        setOnAudioProgressUpdateListener((OnAudioProgressUpdateListener) listener);
+    }
+
+    public class AudioBinder extends Binder {
+        public AudioService getService() {
+            return AudioService.this;
+        }
+    }
+
+    public interface OnAudioProgressUpdateListener {
+        void update(int duration, int currentPosition);
+    }
+}
--- app/src/main/java/com/mzule/audiodemo/MainActivity.java
+++ app/src/main/java/com/mzule/audiodemo/MainActivity.java
@@ -1,39 +1,40 @@
 package com.mzule.audiodemo;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
-public class MainActivity extends ActionBarActivity implements MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
-    private static final int PROGRESS_UPDATE_PERIOD = 100;
-    private static final int MSG_UPDATE_PROGRESS = 1;
+public class MainActivity extends ActionBarActivity implements MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnErrorListener, AudioService.OnAudioProgressUpdateListener {
 
     @InjectView(R.id.progressBar) ProgressBar progressBar;
     @InjectView(R.id.timeView) TextView timeView;
 
     private SimpleDateFormat dateFormat = new SimpleDateFormat("mm:ss");
-    private boolean isStopped;
-    private int duration;
-    private int currentPosition;
-    private MediaPlayer mediaPlayer;
-    private Handler handler = new Handler() {
+    private AudioService audioService;
+    private ServiceConnection conn = new ServiceConnection() {
         @Override
-        public void handleMessage(Message msg) {
-            updatePercent();
-            if (!isStopped) {
-                handler.sendEmptyMessageDelayed(MSG_UPDATE_PROGRESS, PROGRESS_UPDATE_PERIOD);
-            }
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            AudioService.AudioBinder audioBinder = (AudioService.AudioBinder) service;
+            audioService = audioBinder.getService();
+            audioService.setListener(MainActivity.this);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
         }
     };
 
@@ -43,35 +44,40 @@ public class MainActivity extends ActionBarActivity implements MediaPlayer.OnBuf
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setOnBufferingUpdateListener(this);
-        mediaPlayer.setOnCompletionListener(this);
-        mediaPlayer.setOnPreparedListener(this);
-        try {
-            mediaPlayer.setDataSource("http://huohua-static.qiniudn.com/apk_ccnl.mp3");
-            mediaPlayer.prepareAsync();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Intent intent = new Intent(getApplicationContext(), AudioService.class);
+        intent.setAction(AudioService.ACTION_PLAY);
+        intent.putExtra("url", "http://huohua-static.qiniudn.com/apk_ccnl.mp3");
+        bindService(intent, conn, BIND_AUTO_CREATE);
+        startService(intent);
+        Log.d("startService", "startService");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(conn);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        handler.sendEmptyMessage(MSG_UPDATE_PROGRESS);
-        isStopped = false;
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (audioService != null) {
+            audioService.setListener(this);
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        isStopped = true;
-    }
-
-    private void updatePercent() {
-        currentPosition = mediaPlayer.getCurrentPosition();
-        progressBar.setProgress((int) (currentPosition * 100f / duration));
-        timeView.setText(format(currentPosition) + "/" + format(duration));
+        if (audioService != null) {
+            audioService.setListener(null);
+        }
     }
 
     private String format(int duration) {
@@ -84,13 +90,14 @@ public class MainActivity extends ActionBarActivity implements MediaPlayer.OnBuf
     }
 
     @Override
-    public void onCompletion(MediaPlayer mp) {
-        mp.start();
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        Toast.makeText(getApplicationContext(), "error playing audio", Toast.LENGTH_SHORT).show();
+        return false;
     }
 
     @Override
-    public void onPrepared(MediaPlayer mp) {
-        mp.start();
-        duration = mediaPlayer.getDuration();
+    public void update(int duration, int currentPosition) {
+        progressBar.setProgress((int) (currentPosition * 100f / duration));
+        timeView.setText(format(currentPosition) + "/" + format(duration));
     }
 }
