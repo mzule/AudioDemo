78b30aebfe6e7e1f6935bd01514fa1cf652ff5bc
Jason Cao <mzule4j@gmail.com>
2014-12-23 16:36:37 +0800

    audio play with notification control

--- app/src/main/java/com/mzule/audiodemo/AudioService.java
+++ app/src/main/java/com/mzule/audiodemo/AudioService.java
@@ -1,6 +1,10 @@
 package com.mzule.audiodemo;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Binder;
@@ -8,6 +12,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import java.io.IOException;
 
@@ -19,7 +24,10 @@ public class AudioService extends Service implements MediaPlayer.OnCompletionLis
     private static final int MSG_UPDATE_PROGRESS = 1;
 
     private static final String TAG = AudioService.class.getSimpleName();
+    public static final String ACTION_AUDIO_CONTROL = "com.mzule.audiodemo.AUDIO.CONTROL";
     public static final String ACTION_PLAY = "com.mzule.audiodemo.PLAY";
+    public static final String ACTION_PAUSE = "com.mzule.audiodemo.PAUSE";
+    public static final String ACTION_RESUME = "com.mzule.audiodemo.RESUME";
     private MediaPlayer mediaPlayer = new MediaPlayer();
     private MediaPlayer.OnErrorListener onErrorListener;
     private MediaPlayer.OnBufferingUpdateListener onBufferingUpdateListener;
@@ -52,15 +60,43 @@ public class AudioService extends Service implements MediaPlayer.OnCompletionLis
         Log.d(TAG, "onCreate");
     }
 
+    private void showNotification(String action) {
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.item_notification);
+        Intent intent = new Intent(getApplicationContext(), AudioService.class);
+        if (ACTION_PAUSE.equals(action)) {
+            intent.setAction(ACTION_RESUME);
+            remoteViews.setImageViewResource(R.id.actionButton, R.drawable.player_icon_play);
+        } else {
+            intent.setAction(ACTION_PAUSE);
+            remoteViews.setImageViewResource(R.id.actionButton, R.drawable.player_icon_pause);
+        }
+        intent.putExtra("url", "http://huohua-static.qiniudn.com/apk_ccnl.mp3");
+        PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
+
+        Notification notification = new Notification.Builder(getApplicationContext()).setSmallIcon(R.drawable.ic_launcher).setOngoing(true).build();
+        remoteViews.setTextViewText(R.id.textView, "you are best");
+        remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+        notification.contentView = remoteViews;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+
+    private void dismissNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
+    }
+
     @Override
     public int onStartCommand(final Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
+        showNotification(intent.getAction());
         final String url = intent.getStringExtra("url");
-        mediaPlayer.stop();
-        mediaPlayer.reset();
         new Thread(new Runnable() {
             @Override public void run() {
                 if (ACTION_PLAY.equals(intent.getAction())) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
                     Log.d(TAG, "onStartCommand#run#action_play");
                     try {
                         mediaPlayer.setDataSource(url);
@@ -74,6 +110,12 @@ public class AudioService extends Service implements MediaPlayer.OnCompletionLis
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                } else if (ACTION_PAUSE.equals(intent.getAction())) {
+                    Log.d(TAG, "onStartCommand#run#action_pause");
+                    mediaPlayer.pause();
+                } else if (ACTION_RESUME.equals(intent.getAction())) {
+                    Log.d(TAG, "onStartCommand#run#action_resume");
+                    mediaPlayer.start();
                 }
             }
         }).start();
@@ -85,6 +127,7 @@ public class AudioService extends Service implements MediaPlayer.OnCompletionLis
         isComplete = true;
         stopSelf();
         Log.d(TAG, "onCompletion");
+        dismissNotification();
     }
 
     @Override
